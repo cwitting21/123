8a594232ba4e4b51d926f1a2b3bd983b67e29bbc
Whis Maykit <wmaykit@at-p6.21-school.ru>
2019-06-17 18:39:18 +0300

    FULL

--- Fillit_v2.5/src/fillit.c
+++ Fillit_v2.5/src/fillit.c
@@ -6,13 +6,13 @@
 /*   By: wmaykit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 21:00:23 by wmaykit           #+#    #+#             */
-/*   Updated: 2019/06/16 05:38:00 by cwitting         ###   ########.fr       */
+/*   Updated: 2019/06/17 15:19:34 by wmaykit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int		clean(t_matrix **root, int **figures)
+/*static int		clean(t_matrix **root, int **figures)
 {
 	if (root)
 	{
@@ -25,7 +25,7 @@ static int		clean(t_matrix **root, int **figures)
 		*figures = NULL;
 	}
 	return (0);
-}
+}*/
 
 static t_dlx	*new_box(int edge)
 {
@@ -39,32 +39,6 @@ static t_dlx	*new_box(int edge)
 	new->equal = 0;
 	new->edge = edge;
 	return (new);
-}
-
-static int		options(t_dlx *box, int opt)
-{
-	t_matrix	*root;
-	int			i;
-
-	i = 0;
-	root = NULL;
-	box->edge = 5;
-	if (opt == 1)
-		while (i < box->figures[0])
-			ft_printbits(box->figures[++i], 32);
-	if (opt == 2)
-	{
-		if (!buildmatrix(box))
-			return (clean(NULL, &box->figures));
-		else
-		{
-			printmatrix(root);
-			clean(&root, &box->figures);
-		}
-		free(box);
-	}
-	clean(NULL, &box->figures);
-	return (1);
 }
 
 static int		clean_box(t_dlx **box)
@@ -84,6 +58,26 @@ static int		clean_box(t_dlx **box)
 	return (0);
 }
 
+static int		options(t_dlx *box, int opt)
+{
+	int			i;
+
+	i = 0;
+	box->edge = 5;
+	if (opt == 1)
+		while (i < box->figures[0])
+			ft_printbits(box->figures[++i], 32);
+	if (opt == 2)
+	{
+		if (!buildmatrix(box))
+			return (clean_box(&box));
+		else
+			printmatrix(box->knut);
+	}
+	clean_box(&box);
+	return (1);
+}
+
 int				fillit(const int fd, int opt)
 {
 	char		*read;
@@ -94,7 +88,7 @@ int				fillit(const int fd, int opt)
 			(!(box->figures = identify(ft_read(fd, &read), &read))))
 		return (clean_box(&box));
 	if (opt == 1 || opt == 2)
-		return (options(NULL, opt));
+		return (options(box, opt));
 	if (!solve(&box))
 		return (clean_box(&box));
 	return (1);
